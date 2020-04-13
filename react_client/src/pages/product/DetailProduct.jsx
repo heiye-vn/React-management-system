c03205d8a704a2b43faeb1205f115b89d16cd2c8
heiye-vn <1064239893@qq.com>
2020-04-13 13:45:16 +0800

    商品详情组件

---
+++ react_client/src/pages/product/DetailProduct.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class DetailProduct extends Component {
+    render() {
+        return (
+            <div>
+                DetailProduct
+            </div>
+        );
+    }
+}
+
+export default DetailProduct;
