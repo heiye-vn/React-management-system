68a6d4f885af0bf579a652df58436c798c8d1a32
heiye-vn <1064239893@qq.com>
2020-04-20 13:53:27 +0800

    角色数据模型

---
+++ react_server/schema/roleSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+function formate(date){
+    return date.toLocalString
+}
+
+// 设置角色的数据格式
+const roleSchema = new mongoose.Schema({
+    name:String,
+    create_time:{           // 角色创建的初始时间
+        type:String,
+        // default:new Date()
+    },
+    auth_name:String,       // 授权人
+    auth_time:String,       // 授权时间
+    menus:{                 // 角色的权限（可以访问的一些页面）
+        type:Array,
+        default:[]
+    },            
+})
+
+// project 数据库下的 roles 使用 roleSchema 这个数据格式
+module.exports = mongoose.model('roles',roleSchema)
