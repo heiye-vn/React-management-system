1e8bc4b18adb426b31a28c1162ae6da86b2c4c2e
heiye-vn <1064239893@qq.com>
2020-04-20 13:52:43 +0800

    角色管理相关逻辑

---
+++ react_server/controller/role.js
@@ -0,0 +1,46 @@
+//引入角色模型
+const Roles = require('../schema/roleSchema')
+
+//添加角色
+exports.addRole = async cxt => {
+    
+    const {roleName} = cxt.request.body
+    // console.log(cxt.request.body,1111,Date.now(),typeof Date.now())
+    const result = await Roles.findOne({ name: roleName })
+    if (result) { //如果找到了 就提示角色名称重复
+        cxt.body = {
+            status: 1,
+            msg: '该角色名称重复'
+        }
+    } else {      //如果没有找到该角色 就添加该角色
+        const role = await Roles.create({ name: roleName,create_time:new Date()})
+        // console.log(role)
+        cxt.body = {
+            status: 0,
+            data: role,
+            msg: '添加角色成功',
+        }
+    }
+}
+
+//获取所有的角色集合
+exports.getRoles = async cxt => {
+    // console.log(cxt.query)
+    //找到所有的角色信息
+    const result = await Roles.find()
+    // console.log(result)
+    if (result.length) {   //如果找到了角色集合  那就把集合返回给前台
+        cxt.body = {
+            status: 0,
+            msg: '获取角色成功',
+            data: result
+        }
+    }else{
+        cxt.body = {
+            status: 1,
+            msg: '获取角色列表失败',
+        }
+    }
+
+}
+
